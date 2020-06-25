34dec170bbd47024a65b57db4aa697276969d79f
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-25 22:04:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   validate :is_title_case 
   
   before_validation :make_title_case
+  
+  before_save :email_author_about_post
 
   private
 
@@ -12,6 +14,9 @@ class Post < ActiveRecord::Base
       errors.add(:title, "Title must be in title case")
     end
   end
+  
+  def email_author_about_post 
+  end 
 
   def make_title_case
     self.title = self.title.titlecase
